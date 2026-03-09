68c36a93190bee8d76b4920ae0f7e019567e2be0
JUNO_OKYO <junookyo@gmail.com>
2026-03-10 02:23:17 +0700

    fix(world-clock): add missing category field

--- src/views/world-clock/meta.ts
+++ src/views/world-clock/meta.ts
@@ -5,6 +5,7 @@ const meta: PageMeta = {
   description: 'Xem giờ hiện tại ở nhiều thành phố trên thế giới theo thời gian thực.',
   author: 'KingCong Studio',
   facebook: 'https://kingcongstudio.com',
+  category: 'tool',
 }
 
 export default meta
